f2fe2e1e0f64d56121e8833706a6b98a4b3d57dc
CLN <cnuenthel@gmail.com>
2023-03-24 22:01:22 -0500

    Init Commit

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import cv2
 import pytesseract
 from PIL import Image
+import random
 
+# Install Tesseract OCR from here:
+# https://github.com/UB-Mannheim/tesseract/wiki#windows
 
-image = cv2.imread('/home/pi/Desktop/truck_image.jpg')
+# Load the image and preprocess it
+image = cv2.imread('truck_image.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
+# Save the preprocessed image
+# cv2.imwrite(f"processed/saved_image.jpg", thresh)
 
-image = cv2.imread()
+usdot_number = pytesseract.image_to_string(Image.open(f"processed/saved_image.jpg"))
+
+print(usdot_number)
 
 
 
